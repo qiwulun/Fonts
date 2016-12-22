958eb01c076284289e56b473a1024e7a632d8ccb
qiwulun <qiwulun08@gmail.com>
2016-12-22 13:04:17 +0800

    update

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@ mkdir -p /usr/share/fonts/myFonts
 cp ./*.otf /usr/share/fonts/myFonts/
 cp ./*.TTF /usr/share/fonts/myFonts/
 cp ./*.ttf /usr/share/fonts/myFonts/
-cp ./source-code-pro/SourceCodePro-Regular.ttf /usr/share/fonts/myFonts/
 mkfontscale #fonts.scale文件，它用来控制字体旋转缩放）
 mkfontdir #fonts.dir文件，它用来控制字体粗斜体产生）
 fc-cache -fv #建立字体缓存信息
